1d096298e215a0704f6d7d9c1a87edfd17647afe
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-13 12:37:17 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -1,5 +1,44 @@
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.SparkConf
+
+// Number hastags display, Sample interval, App Duration
 object Main extends App {
   println(s"I got executed with ${args size} args, they are: ${args mkString ", "}")
 
-  // your code goes here
+  if (args.length < 4) {
+    System.err.println("Usage: TwitterPopularTags <consumer key> <consumer secret> " +
+      "<access token> <access token secret> [<filters>]")
+    System.exit(1)
+  }
+
+  val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+  val filters = args.takeRight(args.length - 4)
+
+  System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+  System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+  System.setProperty("twitter4j.oauth.accessToken", accessToken)
+  System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+  val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
+  val ssc = new StreamingContext(sparkConf, Seconds(2))
+  val stream = TwitterUtils.createStream(ssc, None, filters)
+
+  val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+  val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+                     .map{case (topic, count) => (count, topic)}
+                     .transform(_.sortByKey(false))
+    
+  topCounts60.foreachRDD(rdd => {
+    val topList = rdd.take(10)
+    println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+    topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+  })
 }
+
+
+// The output of your program should be lists of hashtags that were determined 
+// to be popular during the program's execution, as well as lists of users, 
+// per-hashtag, who were related to them.
