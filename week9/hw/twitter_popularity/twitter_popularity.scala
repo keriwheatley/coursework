bb5a7636a743db8e126e8b1017dc066aaec2deaf
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-13 12:40:24 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -7,11 +7,15 @@ import org.apache.spark.SparkConf
 object Main extends App {
   println(s"I got executed with ${args size} args, they are: ${args mkString ", "}")
 
+  println(args.length)
+
   if (args.length < 4) {
     System.err.println("Usage: TwitterPopularTags <consumer key> <consumer secret> " +
       "<access token> <access token secret> [<filters>]")
     System.exit(1)
   }
+
+  println("Now here 1")
 
   val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
   val filters = args.takeRight(args.length - 4)
@@ -20,6 +24,8 @@ object Main extends App {
   System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
   System.setProperty("twitter4j.oauth.accessToken", accessToken)
   System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+  println("Now here 2")
 
   val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
   val ssc = new StreamingContext(sparkConf, Seconds(2))
@@ -30,6 +36,8 @@ object Main extends App {
   val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
                      .map{case (topic, count) => (count, topic)}
                      .transform(_.sortByKey(false))
+
+  println("Now here 3")
     
   topCounts60.foreachRDD(rdd => {
     val topList = rdd.take(10)
