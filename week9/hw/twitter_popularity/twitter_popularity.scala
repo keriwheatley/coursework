fad8ff59ed1a8e52fb890297f0148ef6511807f7
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-14 17:58:09 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -20,12 +20,15 @@ object Main extends App {
   val numHashtags:Int = 10
   val sampleInterval:Int = 60
   val runDuration:Int = 180
-  println(args(4))
-  println(args(5))
-  println(args(6))
-  // val numHashtags:Int = if (args(4).isEmpty) 10 else args(4)
-  // val sampleInterval:Int = if (args(5).isEmpty) 30 else args(5)
-  // val runDuration:Int = if (args(6).isEmpty) 1800 else args(6)
+  
+  val numHashtagsTEST:Int = if (args(4).isEmpty) 10 else args(4)
+  val sampleIntervalTEST:Int = if (args(5).isEmpty) 30 else args(5)
+  val runDurationTEST:Int = if (args(6).isEmpty) 1800 else args(6)
+
+  println(numHashtagsTEST)
+  println(sampleIntervalTEST)
+  println(runDurationTEST)
+
   println(s"Number hashtags: ${numHashtags}")
   println(s"Length of sample intervals (in seconds): ${sampleInterval}")
   println(s"Duration of program run (in seconds): ${runDuration}")
@@ -42,75 +45,16 @@ object Main extends App {
   val hashtags = stream.flatMap {
     hashtag => hashtag.getHashtagEntities.map(_.getText)} 
   hashtags.print()
-  // tags.countByValue().foreachRDD {rdd => val now = org.joda.time.DateTime.now() rdd.sortBy(_._2) .map(x => (x, now)) }
 
   val tweets = stream.map(status => status.getText())
   tweets.print()
 
-
-
-  //   val topList = rdd.filter(numHashtags)
-  //   println(topList)
-  //   val timeElasped = (System.currentTimeMillis() - startTimeMillis)/1000 
-  //   println(s"\nList of ${numHashtags} most popular topics at ${timeElasped} seconds (%s total):".format(rdd.count()))
-  //   topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-  // })
-
-  // val users = stream.flatMap (status => status.getUser)
-  // users.print()
-
-  // val authors = stream.flatMap {author => author.getName.map(_.getText) }
-  // authors.print()
-
-  // val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
-
-
-  // hashTags.foreachRDD(rdd => {
-  //   val topList = rdd.take(numHashtags)
-  //   println(topList)
-    // val timeElasped = (System.currentTimeMillis() - startTimeMillis)/1000 
-    // println(s"\nList of ${numHashtags} most popular topics at ${timeElasped} seconds (%s total):".format(rdd.count()))
-    // topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-  // })
-
-
-
-  // def updateFunc(values: Seq[Int], state: Option[Int]): Option[Int] = {
-  //     val currentCount = values.sum
-  //     val previousCount = state.getOrElse(0)
-  //     Some(currentCount + previousCount)  
-  // }
-
-  // val topCounts = hashTags.map((a:Int,b:Int)).reduceByKeyAndWindow(a + b, Seconds(sampleInterval), Seconds(10))
-  //                  // .map{case (topic, count) => (count, topic)}
-  //                  // .transform(_.sortByKey(false))
-
-  // topCounts.foreachRDD(rdd => {
-  //   val topList = rdd.take(numHashtags)
-  //   println(topList)
-  //   // val timeElasped = (System.currentTimeMillis() - startTimeMillis)/1000 
-  //   // println(s"\nList of ${numHashtags} most popular topics at ${timeElasped} seconds (%s total):".format(rdd.count()))
-  //   // topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-  // })
 
   ssc.start()
   ssc.awaitTerminationOrTimeout(runDuration * 1000)
   println(s"\nMax duration reached. Ending program.")
   ssc.stop()
 }
-
-
-// The output of your program should be lists of hashtags that were determined 
-// to be popular during the program's execution, as well as lists of users, 
-// per-hashtag, who were related to them.
-
-
-
-// The TOP rankings over the entire 30 minutes
-
-// The top ranking over the last few minutes
-
-
 
 
 
