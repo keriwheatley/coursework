885299ccc8655ccb0c546305e932d306f695c51c
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-14 18:49:35 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -57,13 +57,13 @@ object Main extends App {
   val mentions = stream.map {mention => mention.getUserMentionEntities.map(_.getScreenName)}
   mentions.print()
 
-  stream.foreach { status => {
+  stream.map { status => {
     val statusAuthor = status.getUser().getScreenName()
     val mentionedEntities = status.getUserMentionEntities.map(_.getScreenName).toList
     // val participants = (statusAuthor :: mentionedEntities).toSet - userName
     // val text = participants.map(p=>"@"+p).mkString(" ") + " OK."
     // val reply = new StatusUpdate(text).inReplyToStatusId(status.getId)
-    println("Replying: " + statusAuthor)
+    // println("Replying: " + statusAuthor)
     // twitter.updateStatus(reply)
   }}
 
