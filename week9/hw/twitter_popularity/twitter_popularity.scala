3ac6a16aa98989a7e9b8c7adddf688fd09240108
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-15 15:04:50 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -52,7 +52,7 @@ object Main extends App {
   // hashtagCount.print()
 
   val data = stream.map {line => 
-        List(line.getHashtagEntities.map(_.getText).toList,
+        (line.getHashtagEntities.map(_.getText).toList,
         line.getUser().getScreenName()
         // ,
         // line.getUserMentionEntities.map(_.getScreenName).toList
