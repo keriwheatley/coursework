2e8cb4c4547a896a20918ef5eaf6dd2d1d36ef62
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-15 10:46:24 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -50,9 +50,7 @@ object Main extends App {
 
   val hashtagUpdate = hashtags.map {line => totHashtagCount(line) += 1}
 
-  hashtags.foreachRDD(rdd => {
-              rdd.foreach(line => println("\nTest"))
-              })
+  hashtags.foreach(line => println("\nTest"))
 
   // totHashtagCount.keys.foreach{key => 
     // print("Key = " + key)
