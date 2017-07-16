55d07a87e94d9127af04a030d768a4cab0cfd872
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-16 10:52:15 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.twitter._
 import org.apache.spark.SparkConf
