0277aa6590b22891e0dd0010ba4f362ac7c8b74b
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-15 20:35:51 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -57,7 +57,7 @@ object Main extends App {
 
   data.print()
 
-  val reduce = data.map(line => println(line))
+  val reduce = data.map(line => line.print())
 
     // ).reduceByKey((hashtag,values) => (hashtag, values))
 
