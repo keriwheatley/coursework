e4b99bc08a6494e400d6e2ae20cbcdec3c7cea1c
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-15 21:26:23 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -58,7 +58,7 @@ object Main extends App {
   data.print()
 
   val hashtagCount = data.reduceByKey((hashtag,value) => 
-        (hashtag._1 + value._1,hashtag._1 + value._2,hashtag._1 + value._3))
+        (hashtag._1 + value._1,hashtag._1.concat(value._2),hashtag._1.concat(value._3)))
 
   hashtagCount.print()
 
