5eb0abd34015602a941e60dafa90174a203bc0cb
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-07-16 08:13:05 -0500

    updates to twitter_popularity

--- week9/hw/twitter_popularity/twitter_popularity.scala
+++ week9/hw/twitter_popularity/twitter_popularity.scala
@@ -60,7 +60,7 @@ object Main extends App {
         (hashtag._1 + value._1,hashtag._2 + value._2,hashtag._3 + value._3))
 
   hashtagCount.foreachRDD(rdd => {
-    val topList = rdd.top(2, key=lambda x: x[1][0])
+    val topList = rdd.sortBy(_._2._1).take(numHashtags)
     println(s"\nPopular topics in last ${sampleInterval} seconds (%s total):".format(rdd.count()))
     topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
     }) 
